e76226e17676a80b5d31b6c8edf1897f96ecf4cd
sobanbadonia07 <166586472+sobanbadonia07@users.noreply.github.com>
2024-05-23 20:57:44 +1000

    Update Ordercreation20log.py

--- Ordercreation20log.py
+++ Ordercreation20log.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
+import os
 import pytest
 import time
 import logging
